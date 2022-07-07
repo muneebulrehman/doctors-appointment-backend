2c7d069bdb0fa24ebf198c7dc447b89cbcaab4b5
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-07 19:38:40 +0530

    Added checks in db for doctors table

--- db/migrate/20220707135451_create_doctors.rb
+++ db/migrate/20220707135451_create_doctors.rb
@@ -1,11 +1,11 @@
 class CreateDoctors < ActiveRecord::Migration[7.0]
   def change
     create_table :doctors do |t|
-      t.string :name
-      t.string :photo
-      t.string :bio
-      t.string :profession
-      t.integer :price
+      t.string :name, null: false
+      t.string :photo, null: false
+      t.string :bio, null: false
+      t.string :profession, null: false
+      t.integer :price, null: false
 
       t.timestamps
     end
