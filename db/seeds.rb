9a0a5bfc924ef7f4310a473bfcb46a82386b14bf
backtofayton <yucelolcay@gmail.com>
2022-07-09 12:46:19 +0300

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user_1 = User.create({name: "bob", email: "bob@fake", password: "123456"})
+user_2 = User.create({name: "jane", email: "jane@fake", password: "123456"})
+
+doctor_1 = Doctor.create({name: "doctor_jivago", photo: "photo_url", bio: "nice doctor", profession: "doctor maker"})
+
+now = DateTime.now
+appointment_1 = Appointment.create({user: user_1, doctor: doctor_1, date: now})
+appointment_2 = Appointment.create({user: user_1, doctor: doctor_1, date: now.next_day})
