df66239f3308b0aae236bc3840b4cc8b8ba5764f
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-11 11:41:55 +0530

    Updated user with username and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_135451) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_060244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_135451) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name", null: false
+    t.string "email", null: false
   end
 
 end
