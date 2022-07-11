081eae55647d65ba5ebe5adb96245534c0496aef
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-11 15:19:39 +0530

    Finished user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,9 @@
 class Api::UsersController < ApplicationController
-
   def create
     @user = User.new(user_params)
     if @user.save
+      @current_user = @user
+      @logged_in = true
       render json: { success: true, message: 'User created successfully' }, status: :created
     else
       render json: { success: false, message: @user.errors.full_messages }, status: :unprocessable_entity
@@ -12,7 +13,8 @@ class Api::UsersController < ApplicationController
   def login
     @user = User.where(user_name: params[:user_name])
     if @user.present?
-      set_current_user(params[:user_name])
+      @current_user = @user
+      @logged_in = true
       render json: { success: true, message: 'User logged in successfully' }, status: :ok
     else
       render json: { success: false, message: 'User not found' }, status: :not_found
@@ -20,7 +22,8 @@ class Api::UsersController < ApplicationController
   end
 
   def logout
-    destroy_current_user
+    @current_user = nil
+    @logged_in = false
     render json: { success: true, message: 'User logged out successfully' }, status: :ok
   end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::API
-  # helper_method :current_user
-  @@CURRENT_USER
-  @@logged_in = false
-  def set_current_user(user)
-    @@CURRENT_USER = User.where(user_name: user)
-    @@logged_in = true
-  end
-
-  def destroy_current_user
-    @@CURRENT_USER = nil
-    @@logged_in = false
-  end
+  @current_user = nil
+  @logged_in = false
 end
