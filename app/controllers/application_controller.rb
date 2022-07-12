5cd96f4c7c99c424f2be66a52f6fa2cf3ff356c8
backtofayton <yucelolcay@gmail.com>
2022-07-12 11:34:53 +0300

    Fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,9 @@ class ApplicationController < ActionController::API
   def authenticate_user
     cookie = request.headers['Cookie']
     cookie_list = cookie&.split(';')
-    user_name_cookie = cookie_list&.select { |co| co.include?('user_name') }
-    if user_name_cookie
-      _user_name_cookies = user_name_cookie[0]&.split('=')
-      @current_user = _user_name_cookies[1] if _user_name_cookies
+    user_name_cookie = cookie_list&.select { |co| co.include?('user_name') }  
+    if user_name_cookie && user_name_cookie[0]&.split('=')
+      @current_user = user_name_cookie[0]&.split('=')[1] 
     end
   end
 end
