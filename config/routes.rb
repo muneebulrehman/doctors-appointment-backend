793c8cb6a9844fc4285d95c56e1a7722da789dbc
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-13 15:35:33 +0530

    Removed old routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
-    # get 'users/create'
-    # get 'users/update'
-    # get 'users/destroy'
     post '/users', to: 'users#create'
     post '/auth', to: 'users#authenticate'
     resources :doctors, only: [:index, :show]
