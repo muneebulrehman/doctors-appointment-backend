6a229d343cafa15beeea2ff0b75194ca2946072d
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-14 11:43:47 +0530

    Added root route to handle heroku error

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   end
 
   resources :appointments, path: "#{api_route}/appointments"
+
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
