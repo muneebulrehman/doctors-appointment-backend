d180aa3af789ee2a5c1f70e25d4cb8892c8a9e74
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-14 14:30:41 +0530

    Added cors policy

--- config/application.rb
+++ config/application.rb
@@ -24,4 +24,10 @@ module DoctorsAppointmentBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', :headers => :any, :methods => [:get, :post, :options]
+    end
+  end
 end
