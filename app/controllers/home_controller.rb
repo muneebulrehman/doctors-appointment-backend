0b16ea9608b8d06418cd3f4f76070221d265463c
backtofayton <yucelolcay@gmail.com>
2022-07-16 21:47:15 +0300

    Cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,4 @@ class HomeController < ApplicationController
   def index
     render json: { message: 'Welcome to the API' }
   end
-
-  def logout
-    de_authenticate_user
-    render json: { success: true, message: 'User logged out successfully' }, status: :ok
-  end
 end
