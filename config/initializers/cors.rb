adae8213de73839cb102f454c188c06eb9edaa54
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-21 16:35:47 +0530

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "*"
     resource "*",
       headers: :any,
-      headers: 'Access-Control-Allow-Origin: *',
+      headers: 'Access-Control-Allow-Origin: https://doctors-appointment-frontend.netlify.app',
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
