d5cf0759e8783041c0f57d05116da096f67c3972
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-21 16:27:27 +0530

    Cors policy fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "*"
     resource "*",
       headers: :any,
+      headers: 'Access-Control-Allow-Origin: *',
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
